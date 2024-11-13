527b381a39636c466df3ee7e340af2471fd588e1
Romain-GUYART <romain.guyart@proton.me>
2024-11-13 11:34:51 +0100

    Initial commit: Docker WordPress & Zabbix

---
+++ install_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt update -y
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+apt update -y
+apt install -y docker-ce docker-ce-cli containerd.io
+
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+docker --version
+docker-compose --version
+
+echo "Docker et Docker Compose installés avec succès !"
